45c79e11ab7977a48d54d436e51f99c4b9b3db71
Ullylaili2710 <79493899+Ullylaili2710@users.noreply.github.com>
2021-05-24 22:17:24 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php 
+$host       = "localhost";
+$user       = "root";
+$password   = "";
+$database   = "db_datacovid"; // Nama database
+$koneksi    = mysqli_connect($host, $user, $password, $database);
+?>
